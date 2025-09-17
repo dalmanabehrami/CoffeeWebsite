85c46b969e3b5054cb5f73e65635d054ce49856a
dalmanabehrami <150936530+dalmanabehrami@users.noreply.github.com>
2025-09-17 15:55:19 +0100

    Fix merge conflict in order.php

--- includes/order_content.php
+++ includes/order_content.php
@@ -11,12 +11,9 @@
         <h1>Order Now!</h1>
 
         <form id="order-form">
-<<<<<<< HEAD
             <!-- Hidden field to send cart JSON -->
             <input type="hidden" name="cart" id="cart-json">
 
-=======
->>>>>>> 1a7c1ca9f0d11617aea35361ea25d40795e70aed
             <div id="message-box" style="margin: 10px auto; max-width: 480px;"></div>
 
             <div class="form-group">
@@ -111,8 +108,4 @@
             </div>
         </form>
     </div>
-<<<<<<< HEAD
 </section>
-=======
-</section>
->>>>>>> 1a7c1ca9f0d11617aea35361ea25d40795e70aed
